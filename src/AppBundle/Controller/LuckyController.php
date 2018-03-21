8ec8605660be58957ac865610af64f7ce8857917
kboygit <heybisayangdako@gmail.com>
2018-03-21 11:48:30 -0700

    created a json response

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,32 @@
+<?php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class LuckyController
+{
+
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = rand(0, 100);
+        return new Response( '<html><body>Lucky number: '.$number.'</body></html>' );
+    }
+    /**
+     *@Route("/api/lucky/number")
+     */
+    public function apiNumberAction()
+    {
+        $data = array(
+          'lucky_number' => rand(0, 100),
+        );
+
+        // calls json_encode() and sets the Content-Type header
+        return new JsonResponse($data);
+    }
+}
