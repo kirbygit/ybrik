0b6ed573d055881993999d8c079817b38bdb2bc5
kboygit <heybisayangdako@gmail.com>
2018-03-23 15:43:12 -0700

    managing the session

--- src/AppBundle/Controller/HelloController.php
+++ src/AppBundle/Controller/HelloController.php
@@ -4,17 +4,25 @@
   use Symfony\Bundle\FrameworkBundle\Controller\Controller;
   use Symfony\Component\HttpFoundation\Response;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+  use Symfony\Component\HttpFoundation\Request;
 
+  /**
+    * @Route("/hello/{firstName}/{lastName}", name="hello")
+    */
   class HelloController extends Controller
   {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-      // retrieve the object from database
-      $product = 'Product';
-      if (!$product) {
-        throw $this->createNotFoundException('The product does not exist');
-      }
-      return $this->render($product);
+      $session = $request->getSession();
+
+      // store an attribute for reuse during a later user request
+      $session->set('foo', 'bar');
+
+      // get the attribute set by another controller in another request
+      $foobar = $session->get('foobar');
+
+      // use a default value if the attribute doesn't exist
+      $filters = $session->get('filters', array());
     }
   }
 ?>
