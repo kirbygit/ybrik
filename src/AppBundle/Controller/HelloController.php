b6051a25d8fcfaa659590ceb79408ec165a09814
kboygit <heybisayangdako@gmail.com>
2018-03-23 11:57:09 -0700

    mapping a url to a controller

---
+++ src/AppBundle/Controller/HelloController.php
@@ -0,0 +1,16 @@
+<?php
+  namespace AppBundle\Controller;
+
+  use Symfony\Component\HttpFoundation\Response;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+  class HelloController
+  {
+    /**
+    * @Route("/hello/{name}", name="hello")
+    */
+    public function indexAction($name)
+    {
+      return new Response('<html><body>Hello '.$name.'!</body></html>'); }
+    }
+?>
