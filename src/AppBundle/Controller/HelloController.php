ef39f695f5817a3deedd943bfa4eb41bf7830482
kboygit <heybisayangdako@gmail.com>
2018-03-23 15:52:23 -0700

    flash messages

--- src/AppBundle/Controller/HelloController.php
+++ src/AppBundle/Controller/HelloController.php
@@ -24,5 +24,23 @@
       // use a default value if the attribute doesn't exist
       $filters = $session->get('filters', array());
     }
+
+    public function updateAction(Request $request)
+    {
+      $form = $this->createForm('Sample Form');
+      $form->handleRequest($request);
+      if ($form->isValid()) {
+        // do some sort of processing
+
+        $this->addFlash(
+          'notice',
+          'Your changes were saved!'
+        );
+        // $this->addFlash is equivalent to $this->get('session')->getFlashBag()->add
+
+        return $this->redirectToRoute('Route');
+      }
+      return $this->render($form);
+    }
   }
 ?>
